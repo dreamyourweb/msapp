e8e4ec9ca1676e0d0b1b09f8593f4048ef9e536a
Teun van Gils <edu@teunvg.nl>
2015-08-29 15:04:11 +0200

    Basic card layout.

--- client/home.js
+++ client/home.js
@@ -1,5 +1,10 @@
 Template.card.helpers({
     icon() {
-        return this.type;
+        let icons = [
+          "warning" => "ion-alert-circled",
+          "question" => "ion-help-circled",
+          "info" => "ion-information-circled"
+        ];
+        return icons[this.type];
     }
 });
