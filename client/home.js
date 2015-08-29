40ac7c776692eac0b88feac754d5cdb89df1396d
Andres Lamont <alamont@gmail.com>
2015-08-29 16:51:44 +0200

    aewfr

--- client/home.js
+++ client/home.js
@@ -1,7 +1,20 @@
+let generateDummyData = (n, init, a) => {
+    output = [init];
+    labels = [-n + 1];
+    for (let i = 1; i < n; i++) {
+        output[i] = output[i - 1] + (Math.random() - 0.5) * 2 * a;
+        labels[i] = -n + i;
+    }
+    return {
+        series: [output],
+        labels: labels
+    };
+};
+
 let stats = {
     temperature: new ReactiveVar(22),
-    heart: new ReactiveVar(55),
-    stress: new ReactiveVar(60),
+    heart: new ReactiveVar(generateDummyData(100, 55, 10)),
+    stress: new ReactiveVar(generateDummyData(100, 20, 2)),
     respiration: new ReactiveVar(13)
 };
 
@@ -10,18 +23,53 @@ let stats = {
 // }, 2500);
 
 Template.stats.helpers({
-    temperature() { return stats.temperature.get() },
-    heart() { return stats.heart.get() },
-    stress() { return stats.stress.get() },
-    respiration() { return stats.respiration.get() }
+    temperature() {
+        return stats.temperature.get()
+    },
+    heart() {
+        return stats.heart.get()
+    },
+    stress() {
+        return stats.stress.get()
+    },
+    respiration() {
+        return stats.respiration.get()
+    }
 });
+
+Template.stats.rendered = () => {
+    let chartOptions = {
+        fullWidth: true,
+        axisX: {
+            showGrid: false,
+            showLabel: false
+        },
+        axisY: {
+            showGrid: false,
+            showLabel: false
+        },
+        showPoint: false,
+        height: 50,
+        chartPadding: {
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0
+        }
+    };
+
+    new Chartist.Line('.stressChart', stats.stress.get(), chartOptions);
+
+    new Chartist.Line('.heartChart', stats.heart.get(), chartOptions);
+
+};
 
 Template.card.helpers({
     icon() {
         let icons = {
-          "warning": "ion-alert-circled",
-          "question": "ion-help-circled",
-          "info": "ion-information-circled"
+            "warning": "ion-alert-circled",
+            "question": "ion-help-circled",
+            "info": "ion-information-circled"
         };
         return icons[this.type];
     }
