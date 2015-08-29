072f030cf31282686cc5012589765aef3b52cb6b
Andres Lamont <alamont@gmail.com>
2015-08-29 17:49:43 +0200

    styling

--- client/home.js
+++ client/home.js
@@ -13,24 +13,32 @@ let generateDummyData = (n, init, a) => {
 
 let stats = {
     temperature: new ReactiveVar(22),
-    heart: new ReactiveVar(generateDummyData(100, 55, 10)),
+    heart: new ReactiveVar(generateDummyData(100, 55, 2)),
     stress: new ReactiveVar(generateDummyData(100, 20, 2)),
     respiration: new ReactiveVar(13)
 };
 
-// Meteor.setInterval(() => {
-//     stats.temperature.
-// }, 2500);
+Meteor.setInterval(() => {
+    let h = stats.heart.get();
+    h.series[0].splice(0, 1);
+    h.series[0].push(h.series[0][98] + (Math.random()-0.5) * 2 * 2);
+    stats.heart.set(h);
+
+    let s = stats.stress.get();
+    s.series[0].splice(0, 1);
+    s.series[0].push(s.series[0][98] + (Math.random()-0.5) * 2 * 2);
+    stats.stress.set(s);
+}, 1000);
 
 Template.stats.helpers({
     temperature() {
         return stats.temperature.get()
     },
     heart() {
-        return stats.heart.get()
+        return Math.round(stats.heart.get().series[0][stats.heart.get().series[0].length - 1]);
     },
     stress() {
-        return stats.stress.get()
+        return Math.round(stats.stress.get().series[0][stats.stress.get().series[0].length - 1]);
     },
     respiration() {
         return stats.respiration.get()
@@ -53,21 +61,25 @@ Template.stats.rendered = () => {
         chartPadding: {
             top: 0,
             right: 0,
-            bottom: 0,
-            left: 0
+            bottom: -25,
+            left: -25
         }
     };
 
-    new Chartist.Line('.stressChart', stats.stress.get(), chartOptions);
+    let stressChart = new Chartist.Line('.stressChart', stats.stress.get(), chartOptions);
+    let heartChart = new Chartist.Line('.heartChart', stats.heart.get(), chartOptions);
 
-    new Chartist.Line('.heartChart', stats.heart.get(), chartOptions);
+    Template.instance().autorun(() => {
+        stressChart.update(stats.stress.get());
+        heartChart.update(stats.heart.get());
+    });
 
 };
 Template.registerHelper("color", function() {
     let colors = {
-      "warning": "energized",
-      "question": "calm",
-      "info": "positive"
+        "warning": "energized",
+        "question": "calm",
+        "info": "positive"
     };
     return colors[this.type];
 })
@@ -82,11 +94,11 @@ Template.card.helpers({
         return icons[this.type];
     },
     action() {
-      let actions = {
-        "warning": "no-action",
-        "question": "slider-action",
-        "info": "no-action"
-      };
-      return actions[this.type];
+        let actions = {
+            "warning": "no-action",
+            "question": "slider-action",
+            "info": "no-action"
+        };
+        return actions[this.type];
     }
 });
