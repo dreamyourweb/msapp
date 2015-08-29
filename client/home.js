2e9b6d4b617270503ff09b709343c7eb0df2c3d7
Andres Lamont <alamont@gmail.com>
2015-08-29 15:06:23 +0200

    ARgaerg

--- client/home.js
+++ client/home.js
@@ -1,3 +1,21 @@
+let stats = {
+    temperature: new ReactiveVar(22),
+    heart: new ReactiveVar(55),
+    stress: new ReactiveVar(60),
+    respiration: new ReactiveVar(13)
+};
+
+// Meteor.setInterval(() => {
+//     stats.temperature.
+// }, 2500);
+
+Template.stats.helpers({
+    temperature() { return stats.temperature.get() },
+    heart() { return stats.heart.get() },
+    stress() { return stats.stress.get() },
+    respiration() { return stats.respiration.get() }
+});
+
 Template.card.helpers({
     icon() {
         return this.type;
