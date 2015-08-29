f1cb3c017f38ec432240db23d98c5ef2e93e451c
Andres Lamont <alamont@gmail.com>
2015-08-29 15:19:05 +0200

    stats stuff

--- collections/cards.js
+++ collections/cards.js
@@ -3,18 +3,15 @@ Cards = new Mongo.Collection("cards");
 let types = ["warning", "question", "info"];
 
 
-Meteor.startup(() => {
-    console.log(Cards.find().count());
-    if (Cards.find().count() === 0) {
+if (Cards.find().count() === 0 && Meteor.isServer) {
 
-        for(let i = 0; i < 10; i++){
-            Cards.insert({
-                type: types[Math.floor(Math.random()*3)],
-                title: "Kaart " + i + ":",
-                message: "Bla bla bla " + i,
-                status: "new"
-            });
-        }
-
+    for(let i = 0; i < 10; i++){
+        Cards.insert({
+            type: types[Math.floor(Math.random()*3)],
+            title: "Kaart " + i + ":",
+            message: "Bla bla bla " + i,
+            status: "new"
+        });
     }
-});
+
+}
